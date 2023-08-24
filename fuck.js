37a7cab5c72d4cf00d0c3cd44f45f2507bda68f1
Sohardb <141365264+Sohardb@users.noreply.github.com>
2023-08-24 21:49:13 +0530

    Add files via upload

---
+++ fuck.js
@@ -0,0 +1,33 @@
+
+const myFunction =()=>{
+    const a=document.querySelector(".wrapper").classList;
+    a.add("active-popup");
+    console.log("done")
+}
+
+const myfunction =()=>{
+    const b=document.querySelector(".wrapper").classList;
+    b.remove("active-popup");
+    console.log("done")
+}
+const myff =()=>{
+    const c=document.querySelector(".wrapper").classList;
+    c.add("active");
+    console.log("done")
+}
+const myf=()=>{
+    const x=document.querySelector(".wrapper").classList;
+    x.remove("active");
+    console.log("done")
+}
+const myfunc =()=>{
+    const d=document.querySelector(".wrapper").classList;
+    d.add("active-forget");
+    console.log("done")
+}
+const ff =()=>{
+    const e=document.querySelector(".wrapper").classList;
+    e.remove("active-forget");
+    console.log("done")
+}
+    
